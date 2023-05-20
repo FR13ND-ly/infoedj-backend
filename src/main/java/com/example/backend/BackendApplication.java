313a2c2e23933f4c02ccd3b9ecddb8fd6c30b80b
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2023-05-20 10:18:20 +0300

    Update BackendApplication.java

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -19,7 +19,7 @@ public class BackendApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://cylinuxish.s3-website.eu-central-1.amazonaws.com"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://cylinuxish.s3-website.eu-central-1.amazonaws.com"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
